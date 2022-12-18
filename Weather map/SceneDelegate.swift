0c73d79c12095031109e0ecd5d8dec2e14143f4d
Kurkin Oleksii <oleksii.kurkin@airslate.com>
2022-12-18 20:15:37 +0200

    Xcode project initial commit

---
+++ Weather map/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Weather map
+//
+//  Created by Oleksii on 18.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ Weather map/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Weather map
+//
+//  Created by Oleksii on 18.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
